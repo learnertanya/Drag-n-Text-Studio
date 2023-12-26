bd06eda5146137fe85d1416e22345e8cae104026
Tanya <94377598+learnertanya@users.noreply.github.com>
2023-12-26 22:00:29 +0530

    styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Stage, Layer, Text } from 'react-konva';
+// App.js
+import React, { useState, useRef, useEffect } from "react";
+import { Stage, Layer, Text } from "react-konva";
+import "./App.css";
 
 const App = () => {
-  const [text, setText] = useState('Type your text here');
-  const [textColor, setTextColor] = useState('#000000');
+  const [text, setText] = useState("Type your text here");
+  const [textColor, setTextColor] = useState("#000000");
   const [fontSize, setFontSize] = useState(20);
-  const [fontFamily, setFontFamily] = useState('Arial');
-  const [textHistory, setTextHistory] = useState([{ text, textColor, fontSize, fontFamily }]);
+  const [fontFamily, setFontFamily] = useState("Arial");
+  const [textHistory, setTextHistory] = useState([
+    { text, textColor, fontSize, fontFamily },
+  ]);
   const [positionHistory, setPositionHistory] = useState([{ x: 0, y: 0 }]);
   const [currentStep, setCurrentStep] = useState(0);
   const stageRef = useRef(null);
 
   useEffect(() => {
     const newTextState = { text, textColor, fontSize, fontFamily };
-    const newPositionState = { x: stageRef.current.findOne('Text').x(), y: stageRef.current.findOne('Text').y() };
+    const newPositionState = {
+      x: stageRef.current.findOne("Text").x(),
+      y: stageRef.current.findOne("Text").y(),
+    };
 
     setTextHistory((prev) => [...prev.slice(0, currentStep + 1), newTextState]);
-    setPositionHistory((prev) => [...prev.slice(0, currentStep + 1), newPositionState]);
+    setPositionHistory((prev) => [
+      ...prev.slice(0, currentStep + 1),
+      newPositionState,
+    ]);
     setCurrentStep((prev) => prev + 1);
   }, [text, textColor, fontSize, fontFamily]);
 
@@ -61,60 +71,85 @@ const App = () => {
     const { text, textColor, fontSize, fontFamily } = textHistory[step];
     const { x, y } = positionHistory[step];
 
+    const isSpacePresent = text.includes(" ");
+
     setText(text);
     setTextColor(textColor);
     setFontSize(fontSize);
     setFontFamily(fontFamily);
 
-    if (stageRef.current) {
-      stageRef.current.findOne('Text').position({ x, y });
-      stageRef.current.batchDraw();
+    if (isSpacePresent) {
+      if (stageRef.current) {
+        stageRef.current.findOne("Text").position({ x, y });
+        stageRef.current.batchDraw();
+      }
     }
   };
 
   return (
-    <div>
-      <div>
-        <label>Text:</label>
-        <input type="text" value={text} onChange={handleTextChange} />
-      </div>
-      <div>
-        <label>Text Color:</label>
-        <input type="color" value={textColor} onChange={handleTextColorChange} />
-      </div>
-      <div>
-        <label>Font Size:</label>
-        <input type="number" value={fontSize} onChange={handleFontSizeChange} />
-      </div>
-      <div>
-        <label>Font Family:</label>
-        <select value={fontFamily} onChange={handleFontFamilyChange}>
-          <option value="Arial">Arial</option>
-          <option value="Verdana">Verdana</option>
-          <option value="Times New Roman">Times New Roman</option>
-        </select>
-      </div>
-      <div>
+    <div className="container">
+      <div className="toolbar">
         <button onClick={handleUndo} disabled={currentStep === 0}>
           Undo
         </button>
-        <button onClick={handleRedo} disabled={currentStep === textHistory.length - 1}>
+        <button
+          onClick={handleRedo}
+          disabled={currentStep === textHistory.length - 1}
+        >
           Redo
         </button>
       </div>
-      <Stage width={window.innerWidth} height={window.innerHeight} ref={stageRef}>
-        <Layer>
-          <Text
-            text={text}
-            fill={textColor}
-            fontSize={fontSize}
-            fontFamily={fontFamily}
-            draggable
-            dragBoundFunc={(pos) => ({ x: pos.x, y: pos.y })}
-            onDragEnd={handleTextDragEnd}
-          />
-        </Layer>
-      </Stage>
+      <div className="main-content">
+        <div className="text-options">
+          <label>Text:</label>
+          <input type="text" value={text} onChange={handleTextChange} />
+        </div>
+        <div className="canvas">
+          <Stage
+            width={window.innerWidth - 300}
+            height={window.innerHeight - 100}
+            ref={stageRef}
+          >
+            <Layer>
+              <Text
+                text={text}
+                fill={textColor}
+                fontSize={fontSize}
+                fontFamily={fontFamily}
+                draggable
+                dragBoundFunc={(pos) => ({ x: pos.x, y: pos.y })}
+                onDragEnd={handleTextDragEnd}
+              />
+            </Layer>
+          </Stage>
+        </div>
+        <div className="options">
+          <div className="option-group">
+            <label>Text Color:</label>
+            <input
+              type="color"
+              value={textColor}
+              onChange={handleTextColorChange}
+            />
+          </div>
+          <div className="option-group">
+            <label>Font Size:</label>
+            <input
+              type="number"
+              value={fontSize}
+              onChange={handleFontSizeChange}
+            />
+          </div>
+          <div className="option-group">
+            <label>Font Family:</label>
+            <select value={fontFamily} onChange={handleFontFamilyChange}>
+              <option value="Arial">Arial</option>
+              <option value="Verdana">Verdana</option>
+              <option value="Times New Roman">Times New Roman</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
